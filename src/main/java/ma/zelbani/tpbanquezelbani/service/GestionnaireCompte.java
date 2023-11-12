418a614f4354f39e458125cea7447d9455ad9471
ayoub zelbani <ayoub.zelbani2@gmail.com>
2023-11-12 14:58:46 +0100

    Écriture d'un bean CDI façade, GestionnaireCompte et Écriture d'un bean CDI Init

---
+++ src/main/java/ma/zelbani/tpbanquezelbani/service/GestionnaireCompte.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ma.zelbani.tpbanquezelbani.service;
+
+import jakarta.annotation.sql.DataSourceDefinition;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
+import jakarta.persistence.TypedQuery;
+import jakarta.transaction.Transactional;
+import java.util.List;
+import ma.zelbani.tpbanquezelbani.entity.CompteBancaire;
+
+@DataSourceDefinition(
+        className = "com.mysql.cj.jdbc.MysqlDataSource",
+        name = "java:app/jdbc/banque",
+        serverName = "localhost",
+        portNumber = 3306,
+        user = "root", // nom et
+        password = "zelbani", // mot de passe que vous avez donnés lors de la création de la base de données
+        databaseName = "banque",
+        properties = {
+            "useSSL=false",
+            "allowPublicKeyRetrieval=true",
+            "driverClass=com.mysql.cj.jdbc.Driver"
+        }
+)
+@ApplicationScoped
+public class GestionnaireCompte {
+
+    @PersistenceContext(unitName = "banquePU")
+    private EntityManager em;
+
+    @Transactional
+    public void creerCompte(CompteBancaire c) {
+        em.persist(c);
+    }
+
+    public List<CompteBancaire> getAllComptes() {
+        TypedQuery<CompteBancaire> query = em.createNamedQuery("CompteBancaire.findAll", CompteBancaire.class);
+        return query.getResultList();
+    }
+    
+    public long nbComptes() {
+        Query query = em.createQuery("SELECT COUNT(c) FROM CompteBancaire c");
+        return (long) query.getSingleResult();
+    }
+}
